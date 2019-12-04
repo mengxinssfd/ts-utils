0f387e699d69401d7c4aabafd2a0f666788fd12f
dyh <dyh@xinzon32177@164.com.com>
2019-12-04 18:02:04 +0800

    add function eventProxy

--- src/dom.ts
+++ src/dom.ts
@@ -1,3 +1,5 @@
+import {typeOf} from "./index";
+
 let elementStyle = document.createElement('div').style;
 let vendor = ((): string | false => {
     let transformName: any = {
@@ -5,9 +7,8 @@ let vendor = ((): string | false => {
         Moz: 'MozTransform',
         O: 'OTransform',
         ms: 'msTransform',
-        standard: 'transform'
+        standard: 'transform',
     };
-
     for (let key in transformName) {
         if (elementStyle[transformName[key]] !== undefined) {
             return key;
@@ -15,17 +16,15 @@ let vendor = ((): string | false => {
     }
     return false;
 })();
-
-export const isDom = (function () {
+export const isDom: (target: any) => target is HTMLElement = (function () {
     return (typeof HTMLElement === 'object') ?
-        function (target: any): boolean {
+        function (target: any): target is HTMLElement {
             return target instanceof HTMLElement;
         } :
-        function (target: any): boolean {
+        function (target: any): target is HTMLElement {
             return target && typeof target === 'object' && target.nodeType === 1 && typeof target.nodeName === 'string';
         };
 })();
-
 export const addClass = (function () {
     return !!document.documentElement.classList ? function (target: any, className: string | string[]) {
         target.classList.add(...Array.isArray(className) ? className : [className]);
@@ -33,13 +32,11 @@ export const addClass = (function () {
     } : function (target: any, className: string | string[]) {
         const originClass = target.className;
         const originClassArr = originClass.split(" ");
-
         className = Array.isArray(className) ? className : [className];
         className = [...new Set(className)];
         className = className.filter(cname => !originClassArr.includes(cname));
         if (!className.length) return originClass;
         className = className.join(" ");
-
         target.className = !!originClass ? originClass + " " + className : className;
         return target.className;
     };
@@ -63,10 +60,45 @@ export function prefixStyle(style: string): string | false {
     if (vendor === false) {
         return false;
     }
-
     if (vendor === 'transform') {
         return style;
     }
-
     return vendor + style.charAt(0).toUpperCase() + style.substr(1);
 }
+
+/**
+ * 事件代理
+ * @param containerEl
+ * @param eventType
+ * @param targetEl
+ * @param callback
+ */
+export function eventProxy(
+    containerEl: string | HTMLElement | null,
+    eventType: "string",
+    targetEl: string | HTMLElement,
+    callback: (e: Event) => void,
+): () => void {
+    let containsDom: HTMLElement;
+    if (!containerEl) {
+        containsDom = document.documentElement;
+    } else if (isDom(containerEl)) {
+        containsDom = containerEl;
+    } else {
+        containsDom = document.querySelector(containerEl);
+    }
+
+    function handle(e) {
+        e = e || window.event;
+        let targetDom = isDom(targetEl) ? [targetEl] : Array.from(document.querySelectorAll(targetEl));
+        if (targetDom.includes(e.target)) {
+            callback(e);
+        }
+    }
+
+    containsDom.addEventListener(eventType, handle);
+    return function () {
+        containsDom.removeEventListener(eventType, handle);
+    };
+}
+
