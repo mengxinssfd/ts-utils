fd0858dc49240e47f412d60fe8496910a84a7812
dyh <dyh@xinzon32177@164.com.com>
2019-12-20 18:07:17 +0800

    191220

--- src/array.ts
+++ src/array.ts
@@ -1,7 +1,7 @@
 /**
  * ie9不支持的数组函数
  */
-import {isEmpty, typeOf} from "./common";
+import {isArrayLike, isEmpty, isFunction, typeOf} from "./common";
 
 /**
  * 创建一个包含开始数字和结束数字的数组 包左不包右: start <= item < (end || start + len)
@@ -26,10 +26,11 @@ export function createArray({start = 0, end, len, callback}: {
     return arr;
 }
 
+// ie9支持
 // forEach(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): void;
 export function forEach(callbackfn: (value: any, index: number, array: any[]) => void, thisArg?: any) {
     const arr = thisArg || this;
-    if (!arr) throw new TypeError();
+    if (!isArrayLike(arr)) throw new TypeError();
     for (let i = 0; i < arr.length; i++) {
         callbackfn(arr[i], i, arr);
     }
@@ -38,7 +39,7 @@ export function forEach(callbackfn: (value: any, index: number, array: any[]) =>
 // filter<S extends T>(callbackfn: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S[]
 export function filter(callbackfn: (value: any, index: number, array: any[]) => boolean, thisArg?: any): any[] {
     const arr = thisArg || this;
-    if (!arr) throw new TypeError();
+    if (!isArrayLike(arr)) throw new TypeError();
     const fList: any[] = [];
     for (let i = 0; i < arr.length; i++) {
         const value = arr[i];
@@ -50,9 +51,9 @@ export function filter(callbackfn: (value: any, index: number, array: any[]) => 
 }
 
 // includes(searchElement: T, fromIndex?: number): boolean
-export function includes(thisArg: Array<any>, searchElement: any, fromIndex = 0): boolean {
+export function includes(searchElement: any, fromIndex = 0, thisArg: Array<any>): boolean {
     const arr = thisArg || this;
-    if (!arr) throw new TypeError();
+    if (!isArrayLike(arr)) throw new TypeError();
     for (let i = fromIndex; i < arr.length; i++) {
         const item = arr[i];
         if (item === searchElement) return true;
@@ -76,4 +77,18 @@ export function keys(target: {} | []): Array<string> {
     return arr;
 }
 
+// find<S extends T>(predicate: (this: void, value: T, index: number, obj: T[]) => value is S, thisArg?: any): S | undefined;
+// find(predicate: (value: T, index: number, obj: T[]) => unknown, thisArg?: any): T | undefined;
+export function find(
+    predicate: (value: any, index: number, obj: any[]) => boolean,
+    thisArg?: any,
+): any | undefined {
+    const arr = thisArg || this;
+    if (!isArrayLike(arr)) throw new TypeError();
+    if (!isFunction(predicate)) return;
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        if (predicate(item, i, arr)) return item;
+    }
+}
 
