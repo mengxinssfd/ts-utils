bd51406927b90bae3c6a6708ddd78cf4901aa3b8
dyh <xinzon32177@163.com>
2021-04-03 02:07:40 +0800

    umd的d.ts移动到types里面

--- lib-umd/clone.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function cloneFunction<T extends Function>(fn: T): T;
-export declare function deepClone<T>(target: T): T;
-export declare function deepCloneBfs<T>(target: T): T;
--- lib-umd/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as all from "./export";
-declare const tsUtils: typeof all;
-export default tsUtils;
