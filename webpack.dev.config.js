53c9f05e01ca1a61f3d720cda3760c69f16669c7
dyh <xinzon32177@163.com>
2021-04-21 02:59:06 +0800

    test(webpack.dev.config.js):添加sourceMap

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,39 +1,40 @@
 const path = require("path");
 const HtmlPlugin = require("html-webpack-plugin");
 const config = {
-    entry: {
-        ImgMerge: "./test/merge-img/index.ts",
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: [
-                    "babel-loader",
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            configFile: "tsconfig.webpack.json",
-                        },
-                    },
-                ],
-                exclude: [path.resolve(__dirname, "node_modules")],
+  entry: {
+    ImgMerge: "./test/merge-img/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          "babel-loader",
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "tsconfig.webpack.json",
             },
+          },
         ],
-    },
-    resolve: {
-        extensions: [".js", ".ts"],
-    },
-    plugins: [
-        new HtmlPlugin({
-            template: "./test/merge-img/index.html",
-            filename: "merge-img.html",
-            chunks: ["ImgMerge"],// 于loader一样，在后面的会插到前面去
-        }),
+        exclude: [path.resolve(__dirname, "node_modules")],
+      },
     ],
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: "./test/merge-img/index.html",
+      filename: "merge-img.html",
+      chunks: ["ImgMerge"],// 于loader一样，在后面的会插到前面去
+    }),
+  ],
 };
 module.exports = config;
