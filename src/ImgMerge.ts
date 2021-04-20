78551546d0334d722703ddb3307ee573e05b270b
dyh <xinzon32177@163.com>
2021-04-21 02:57:48 +0800

    feat(ImgMerge.ts):
    1.宽高设置移动到Style
    2.添加图层类
    3.添加图层子类
    4.每添加一个比前面图层的zIndex小的图层，都会重绘一次
    5.addItem函数分解

--- src/ImgMerge.ts
+++ src/ImgMerge.ts
@@ -1,20 +1,62 @@
+import {arrayRemoveItem, insertToArrayRight} from "./array";
 import {assign} from "./object";
-import {isPromiseLike} from "./type";
+import {isNumber, isPromiseLike} from "./type";
 import {loadImg} from "./dom";
 
-type Location = {
+type Style = {
     left?: number;
     right?: number;
     top?: number;
     bottom?: number;
+    width?: number;
+    height?: number;
+    zIndex?: number;
+}
+let id = 0;
+
+class LayerElement {
+    id!: number;
+
+    constructor(public style: Style, public content: HTMLImageElement) {
+        this.id = id++;
+    }
 }
 
-type Size = [number?, number?]
+class Layer {
+    list: LayerElement[] = [];
+
+    add(el: LayerElement) {
+        el.style.zIndex = el.style.zIndex ?? 0;
+        const list = this.list;
+        if (!list.length) {
+            return this.list.push(el);
+        } else {
+            return insertToArrayRight(el, (v, k) => {
+                return v.style.zIndex! <= el.style.zIndex! || k === 0;
+            }, list, true);
+        }
+    }
+
+    remove(el: LayerElement): LayerElement | void
+    remove(index: number): LayerElement | void
+    remove(value) {
+        if (isNumber(value)) {
+            return this.list.splice(value, 1)[0];
+        }
+
+        return arrayRemoveItem(value, this.list);
+    }
+
+    clear() {
+        this.list = [];
+    }
+}
 
 export class MergeImg {
     private _ctx?: CanvasRenderingContext2D;
     private canvas?: HTMLCanvasElement;
     private readonly parent: Element;
+    private layer = new Layer();
 
     constructor(readonly width = 0, readonly height = 0) {
         const parent = document.body;
@@ -46,24 +88,24 @@ export class MergeImg {
         return mi;
     }
 
-    async addImg(url: string, location?: Location, size?: Size): Promise<HTMLImageElement>
-    async addImg(promiseImg: Promise<HTMLImageElement>, location?: Location, size?: Size): Promise<HTMLImageElement>
-    async addImg(urlOrPromiseImg, location: Location = {}, size: Size = []) {
+    render(item: LayerElement) {
         if (!this._ctx) throw new Error();
-        let img: HTMLImageElement;
-        if (isPromiseLike(urlOrPromiseImg as Promise<HTMLImageElement>)) {
-            img = await urlOrPromiseImg;
-        } else {
-            img = await loadImg(urlOrPromiseImg);
-        }
         const ctx = this._ctx;
-        let {left, top, right, bottom} = location;
+        const img = item.content;
+        const {
+            left,
+            top,
+            right,
+            bottom,
+            width,
+            height,
+        } = item.style;
         let dw: number;
         let dh: number;
         let x: number = 0;
         let y: number = 0;
-        dw = img.width;
-        dh = img.height;
+        dw = width || img.width;
+        dh = height || img.height;
 
         // 1.如果设定了宽高，则以设定的宽高为准
         // 2.如果设定了left和right，宽=canvas宽 - left - right
@@ -74,7 +116,7 @@ export class MergeImg {
 
         if (left !== undefined && right !== undefined) {
             x = left;
-            if (size[0] === undefined) {
+            if (width === undefined) {
                 dw = this.width - right - left;
             }
         } else {
@@ -87,7 +129,7 @@ export class MergeImg {
 
         if (top !== undefined && bottom !== undefined) {
             y = top;
-            if (size[1] === undefined) {
+            if (height === undefined) {
                 dh = this.height - top - bottom;
             }
         } else if (top !== undefined) {
@@ -96,7 +138,35 @@ export class MergeImg {
             y = this.height - bottom - dh;
         }
 
-        ctx.drawImage(img, x, y, size[0] ?? dw, size[1] ?? dh);
+        ctx.drawImage(img, x, y, width ?? dw, height ?? dh);
+    }
+
+    reRender() {
+        console.count();
+        this.clear();
+        this.layer.list.forEach(item => this.render(item));
+    }
+
+    clear() {
+        this._ctx!.clearRect(0, 0, this.width, this.height);
+    }
+
+    async addImg(url: string, style?: Style): Promise<HTMLImageElement>
+    async addImg(promiseImg: Promise<HTMLImageElement>, style?: Style): Promise<HTMLImageElement>
+    async addImg(urlOrPromiseImg, style: Style = {}) {
+        let img: HTMLImageElement;
+        if (isPromiseLike(urlOrPromiseImg as Promise<HTMLImageElement>)) {
+            img = await urlOrPromiseImg;
+        } else {
+            img = await loadImg(urlOrPromiseImg);
+        }
+        const item = new LayerElement(style, img);
+        if (this.layer.add(item) !== this.layer.list.length - 1) {
+            this.reRender();
+        } else {
+            this.render(item);
+        }
+
         return img;
     }
 
