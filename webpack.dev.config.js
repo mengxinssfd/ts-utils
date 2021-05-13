3d008ff1ff12c8ecae3c579789b199408d7dc726
dyh <xinzon32177@163.com>
2021-05-13 22:57:37 +0800

    test(webpack.dev.config.js):HtmlPlugin

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,6 +4,7 @@ const config = {
   entry: {
     ImgMerge: "./test/merge-img/index.ts",
     layer: "./test/merge-img/layer.ts",
+    touch: "./test/touch/index.ts",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -31,18 +32,21 @@ const config = {
     extensions: [".js", ".ts"],
   },
   plugins: [
-    new HtmlPlugin([
-      {
-        template: "./test/merge-img/index.html",
-        filename: "merge-img.html",
-        chunks: ["ImgMerge"],// 于loader一样，在后面的会插到前面去
-      },
-      {
-        template: "./test/merge-img/index.html",
-        filename: "layer.html",
-        chunks: ["layer"],// 于loader一样，在后面的会插到前面去
-      },
-    ]),
+    new HtmlPlugin({
+      template: "./test/merge-img/index.html",
+      filename: "merge-img.html",
+      chunks: ["ImgMerge"],// 于loader一样，在后面的会插到前面去
+    }),
+    new HtmlPlugin({
+      template: "./test/merge-img/index.html",
+      filename: "layer.html",
+      chunks: ["layer"],// 于loader一样，在后面的会插到前面去
+    }),
+    new HtmlPlugin({
+      template: "./test/touch/index.html",
+      filename: "touch.html",
+      chunks: ["touch"],// 于loader一样，在后面的会插到前面去
+    }),
   ],
 };
 module.exports = config;
