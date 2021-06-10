0b07d1237aa7e27a1b8923376cbb1908656e9673
DYH <xinzon32177@163.com>
2021-06-10 18:10:19 +0800

    test(url.ts): updateUrlParam

--- __test__/url.test.ts
+++ __test__/url.test.ts
@@ -1,25 +1,13 @@
 import {subString} from "../src/string";
 import {UrlModel} from "../src/UrlModel";
-import {
-    getUrlHash,
-    getUrlHost,
-    getUrlPath,
-    getUrlPort,
-    getUrlProtocol,
-    getUrlParamObj,
-    stringifyUrlSearch,
-    getUrlParam,
-    UrlRegExp,
-    getUrlHashParam,
-    isUrl,
-} from "../src/url";
+import * as u from "../src/url";
 
 const url = "http://www.baidu.com:112332/index.php/admin/MonitorResultManager/monitorData?a%5B%5D=123&a%5B%5D=on&b%5B0%5D=on&b%5B1%5D=on&c=1&c=2&c=3&d=1,2,3,4,5&pid=19&pname=%E7%8E%AF%E7%90%83%E8%B4%B8%E6%98%93%E9%A1%B9%E7%9B%AE%E5%9F%BA%E5%9D%91%E5%9C%B0%E9%93%812%E5%8F%B7%E7%BA%BF%E9%9A%A7%E9%81%93%E7%BB%93%E6%9E%84%E8%87%AA%E5%8A%A8%E5%8C%96%E7%9B%91%E6%B5%8B#test";
 
 
 test("getUrlParamObj", () => {
     const url = "test/aaa=1213123?a=1123&b[0]=1&b[1]=2&d[d]=1&d[e]=2?t=1231&b=123123&a%5B%5D=123&a%5B%5D=on&b%5B0%5D=on&b%5B1%5D=on&c=1&c=2&c=3&d=1,2,3,4,5&pid=19&pname=%E7%8E%AF%E7%90%83%E8%B4%B8%E6%98%93%E9%A1%B9%E7%9B%AE%E5%9F%BA%E5%9D%91%E5%9C%B0%E9%93%812%E5%8F%B7%E7%BA%BF%E9%9A%A7%E9%81%93%E7%BB%93%E6%9E%84%E8%87%AA%E5%8A%A8%E5%8C%96%E7%9B%91%E6%B5%8B#test";
-    const obj = getUrlParamObj(url);
+    const obj = u.getUrlParamObj(url);
     expect(obj).toEqual({
         aaa: "1213123",
         a: ["1123", "123", "on"],
@@ -31,51 +19,51 @@ test("getUrlParamObj", () => {
         pname: "环球贸易项目基坑地铁2号线隧道结构自动化监测"
     });
     expect(obj.pname).toEqual("环球贸易项目基坑地铁2号线隧道结构自动化监测");
-    expect(getUrlParamObj()).toEqual({});
+    expect(u.getUrlParamObj()).toEqual({});
 });
 test("getUrlHashParam", () => {
     const url = "test.com/index?a=param/#/test?a=hash";
-    expect(getUrlHashParam("a", url)).toEqual("hash");
-    expect(getUrlHashParam("a", "test.com/index?a=param")).toEqual("");
-    expect(getUrlHashParam("a")).toEqual("");
+    expect(u.getUrlHashParam("a", url)).toEqual("hash");
+    expect(u.getUrlHashParam("a", "test.com/index?a=param")).toEqual("");
+    expect(u.getUrlHashParam("a")).toEqual("");
 });
 test("getUrlHash", () => {
-    expect(getUrlHash("/index.php#index/admin")).toBe("#index/admin");
-    expect(getUrlHash("/index.php/#/index/admin#test")).toBe("#/index/admin#test");
-    expect(getUrlHash("/index.php")).toBe("");
-    expect(getUrlHash()).toBe("");
+    expect(u.getUrlHash("/index.php#index/admin")).toBe("#index/admin");
+    expect(u.getUrlHash("/index.php/#/index/admin#test")).toBe("#/index/admin#test");
+    expect(u.getUrlHash("/index.php")).toBe("");
+    expect(u.getUrlHash()).toBe("");
 });
 test("getUrlProtocol", () => {
-    expect(getUrlProtocol("file:///E:/wechatCache")).toBe("file");
-    expect(getUrlProtocol("https://www.baidu.com/index")).toBe("https");
-    expect(getUrlProtocol("http://www.baidu.com/index")).toBe("http");
-    expect(getUrlProtocol("/index.php")).toBe("");
-    expect(getUrlProtocol()).toBe("http");
+    expect(u.getUrlProtocol("file:///E:/wechatCache")).toBe("file");
+    expect(u.getUrlProtocol("https://www.baidu.com/index")).toBe("https");
+    expect(u.getUrlProtocol("http://www.baidu.com/index")).toBe("http");
+    expect(u.getUrlProtocol("/index.php")).toBe("");
+    expect(u.getUrlProtocol()).toBe("http");
 });
 test("getUrlHost", () => {
-    expect(getUrlHost("https://www.baidu.com/index")).toBe("www.baidu.com");
-    expect(getUrlHost("https://www.1223-tewre.com/index")).toBe("www.1223-tewre.com");
-    expect(getUrlHost("https://www.1223_tewre.com/index")).toBe("www.1223_tewre.com");
-    expect(getUrlHost("https://www.测试_tewre.com/index")).toBe("www.测试_tewre.com");
-    expect(getUrlHost("https://www.测试-tewre.com/index")).toBe("www.测试-tewre.com");
-    expect(getUrlHost("https://www.-tewre测试.com/index")).toBe("www.-tewre测试.com");
-    expect(getUrlHost("https://www.测试.test.com/index")).toBe("www.测试.test.com");
-    expect(getUrlHost("http://www.baidu.com/index")).toBe("www.baidu.com");
-    expect(getUrlHost("http://www.baidu.com:8080/index")).toBe("www.baidu.com");
-    expect(getUrlHost("file://E:/wechatCache")).toBe("");
-    expect(getUrlHost("/index.php")).toBe("");
-    expect(getUrlHost()).toBe("localhost");
+    expect(u.getUrlHost("https://www.baidu.com/index")).toBe("www.baidu.com");
+    expect(u.getUrlHost("https://www.1223-tewre.com/index")).toBe("www.1223-tewre.com");
+    expect(u.getUrlHost("https://www.1223_tewre.com/index")).toBe("www.1223_tewre.com");
+    expect(u.getUrlHost("https://www.测试_tewre.com/index")).toBe("www.测试_tewre.com");
+    expect(u.getUrlHost("https://www.测试-tewre.com/index")).toBe("www.测试-tewre.com");
+    expect(u.getUrlHost("https://www.-tewre测试.com/index")).toBe("www.-tewre测试.com");
+    expect(u.getUrlHost("https://www.测试.test.com/index")).toBe("www.测试.test.com");
+    expect(u.getUrlHost("http://www.baidu.com/index")).toBe("www.baidu.com");
+    expect(u.getUrlHost("http://www.baidu.com:8080/index")).toBe("www.baidu.com");
+    expect(u.getUrlHost("file://E:/wechatCache")).toBe("");
+    expect(u.getUrlHost("/index.php")).toBe("");
+    expect(u.getUrlHost()).toBe("localhost");
 });
 test("getUrlPath", () => {
-    expect(getUrlPath("https://www.baidu.com/index")).toBe("/index");
-    expect(getUrlPath("https://www.1223-tewre.com/index")).toBe("/index");
-    expect(getUrlPath("https://www.测试.test.com/对方是否")).toBe("/对方是否");
-    expect(getUrlPath("https://www.测试.test.com/index?test=123")).toBe("/index");
-    expect(getUrlPath("https://www.测试.test.com/index/test#test")).toBe("/index/test");
-    expect(getUrlPath("https://www.测试.test.com:8080/index/test#test")).toBe("/index/test");
-    expect(getUrlPath("file:///E:/wechatCache")).toBe("/E:/wechatCache");
-    expect(getUrlPath("/index.php")).toBe("/index.php");
-    expect(getUrlPath()).toBe("/");
+    expect(u.getUrlPath("https://www.baidu.com/index")).toBe("/index");
+    expect(u.getUrlPath("https://www.1223-tewre.com/index")).toBe("/index");
+    expect(u.getUrlPath("https://www.测试.test.com/对方是否")).toBe("/对方是否");
+    expect(u.getUrlPath("https://www.测试.test.com/index?test=123")).toBe("/index");
+    expect(u.getUrlPath("https://www.测试.test.com/index/test#test")).toBe("/index/test");
+    expect(u.getUrlPath("https://www.测试.test.com:8080/index/test#test")).toBe("/index/test");
+    expect(u.getUrlPath("file:///E:/wechatCache")).toBe("/E:/wechatCache");
+    expect(u.getUrlPath("/index.php")).toBe("/index.php");
+    expect(u.getUrlPath()).toBe("/");
 });
 test("urlParse", () => {
     //  a[]=123&a[]=on&b[0]=on&b[1]=on&c=1&c=2&d=1,2,3,4,5&pid=19&pname=环球贸易项目基坑地铁2号线隧道结构自动化监测
@@ -93,16 +81,16 @@ test("urlParse", () => {
     expect(urlParse.hash).toBe("#test");
     expect(urlParse.path).toBe("/index.php/admin/MonitorResultManager/monitorData");
 
-    expect(getUrlHost("/")).toBe("");
-    expect(getUrlPort("/")).toBe("");
-    expect(getUrlPort()).toBe("");
+    expect(u.getUrlHost("/")).toBe("");
+    expect(u.getUrlPort("/")).toBe("");
+    expect(u.getUrlPort()).toBe("");
 
-    expect(getUrlPath("/index.php/admin/MonitorResultManager")).toBe("/index.php/admin/MonitorResultManager");
-    expect(getUrlProtocol("")).toBe("");
-    expect(getUrlHost("/index.php/admin#absdf-23_123")).toBe("");
-    expect(getUrlHost("")).toBe("");
-    expect(getUrlHash("/index.php/admin#absdf-23_123")).toBe("#absdf-23_123");
-    expect(getUrlHash("/index.php/admin")).toBe("");
+    expect(u.getUrlPath("/index.php/admin/MonitorResultManager")).toBe("/index.php/admin/MonitorResultManager");
+    expect(u.getUrlProtocol("")).toBe("");
+    expect(u.getUrlHost("/index.php/admin#absdf-23_123")).toBe("");
+    expect(u.getUrlHost("")).toBe("");
+    expect(u.getUrlHash("/index.php/admin#absdf-23_123")).toBe("#absdf-23_123");
+    expect(u.getUrlHash("/index.php/admin")).toBe("");
 
     const realUrl = "https://www.haodanku.com:80/Openapi/api_detail?id=15#api-parameter";
     const real = new UrlModel(realUrl);
@@ -114,7 +102,7 @@ test("urlParse", () => {
     expect(real.hash).toEqual("#api-parameter");
     expect(real.port).toEqual("80");
     expect(real.toString()).toBe(realUrl);
-    expect(getUrlProtocol("file://test.com")).toEqual("file");
+    expect(u.getUrlProtocol("file://test.com")).toEqual("file");
 
     const empty = new UrlModel("");
     expect(empty.query).toEqual({});
@@ -126,12 +114,12 @@ test("urlParse", () => {
     expect(empty.toString()).toBe("");
 });
 test("queryStringify", () => {
-    expect(stringifyUrlSearch({a: "1123", b: 1123})).toBe("a=1123&b=1123");
-    expect(stringifyUrlSearch({a: "1123", b: 1123, c: undefined})).toBe("a=1123&b=1123");
-    expect(stringifyUrlSearch({a: "1123", b: [1, 2, 3]})).toBe("a=1123&b[0]=1&b[1]=2&b[2]=3");
-    expect(stringifyUrlSearch({a: "1123", b: {d: 1, e: 2}})).toBe("a=1123&b[d]=1&b[e]=2");
-    expect(stringifyUrlSearch({a: "1123", b: [1, 2], d: {d: 1, e: 2}})).toBe("a=1123&b[0]=1&b[1]=2&d[d]=1&d[e]=2");
-    expect(stringifyUrlSearch({a: "1123", b: [1, 2], d: {d: 1, e: undefined}})).toBe("a=1123&b[0]=1&b[1]=2&d[d]=1");
+    expect(u.stringifyUrlSearch({a: "1123", b: 1123})).toBe("a=1123&b=1123");
+    expect(u.stringifyUrlSearch({a: "1123", b: 1123, c: undefined})).toBe("a=1123&b=1123");
+    expect(u.stringifyUrlSearch({a: "1123", b: [1, 2, 3]})).toBe("a=1123&b[0]=1&b[1]=2&b[2]=3");
+    expect(u.stringifyUrlSearch({a: "1123", b: {d: 1, e: 2}})).toBe("a=1123&b[d]=1&b[e]=2");
+    expect(u.stringifyUrlSearch({a: "1123", b: [1, 2], d: {d: 1, e: 2}})).toBe("a=1123&b[0]=1&b[1]=2&d[d]=1&d[e]=2");
+    expect(u.stringifyUrlSearch({a: "1123", b: [1, 2], d: {d: 1, e: undefined}})).toBe("a=1123&b[0]=1&b[1]=2&d[d]=1");
 
     function A() {
         this.a = 1;
@@ -140,14 +128,14 @@ test("queryStringify", () => {
 
     A.prototype.c = 3;
     A.prototype.d = 4;
-    expect(stringifyUrlSearch(new A())).toBe("a=1&b=2");
+    expect(u.stringifyUrlSearch(new A())).toBe("a=1&b=2");
 
-    // TODO parseQuery不能解析成object
-    console.log(getUrlParamObj("a=1123&b[0]=1&b[1]=2&d[d]=1&d[e]=2"));
+    // TODO getUrlParamObj 不能解析成object
+    console.log(u.getUrlParamObj("a=1123&b[0]=1&b[1]=2&d[d]=1&d[e]=2"));
 });
 
 test("getUrlParam", () => {
-    const fn = getUrlParam;
+    const fn = u.getUrlParam;
     const realUrl = "https://www.haodanku.com/Openapi/api_detail?id=15#api-parameter";
 
     expect(fn("id", realUrl)).toEqual("15");
@@ -165,8 +153,15 @@ test("getUrlParam", () => {
     // expect(fn("d[1]", url2)).toEqual("5");
     // expect(fn("d", url2)).toEqual([4, 5]);
 });
+test("updateUrlParam", () => {
+    const fn = u.updateUrlParam;
+    let url = "https://www.haodanku.com/Openapi/api_detail?id=15#api-parameter";
+    url = fn("id", "100", url);
+    expect(url).toEqual("https://www.haodanku.com/Openapi/api_detail?id=100#api-parameter");
+    expect(fn("pid", "15", url)).toEqual(url);
+});
 test("UrlRegExp", () => {
-    const ure = UrlRegExp;
+    const ure = u.UrlRegExp;
     const realUrl = "https://www.haodanku.com/Openapi/api_detail?id=15#api-parameter";
 
     expect(ure.test(realUrl)).toBeTruthy();
@@ -183,7 +178,7 @@ test("UrlRegExp", () => {
     ).not.toEqual(["http://test.com/12/sdffte/index", "https://hello.cn"]);
 });
 test("isUrl", () => {
-    const fn = isUrl;
+    const fn = u.isUrl;
     const realUrl = "https://www.haodanku.com/Openapi/api_detail?id=15";
 
     expect(fn(realUrl)).toBeTruthy();
