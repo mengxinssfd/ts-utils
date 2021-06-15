629589d7740717e575a6e9aacd3d9a1a7743faa2
DYH <xinzon32177@163.com>
2021-06-15 18:09:42 +0800

    refactor(event.ts): isScrollEnd

--- src/event.ts
+++ src/event.ts
@@ -396,11 +396,25 @@ export function isVisible(target: HTMLElement, container: HTMLElement | typeof w
  * @param [direct="vertical"]
  * @param [offset=10]
  */
-export function isScrollEnd(el: HTMLElement, direct: "vertical" | "horizontal" = "vertical", offset = 10) {
-    if (direct === "vertical") {
-        return el.scrollTop >= el.scrollHeight - el.clientHeight - offset;
+export function isScrollEnd(el: HTMLElement | Window, direct = "vertical", offset = 10) {
+    let width: number, height: number;
+    let scrollEl = el as HTMLElement;
+    if (el === window) {
+        let rootEl = document.documentElement;
+        scrollEl = document.body.scrollTop ? document.body : rootEl;
+        width = rootEl.clientWidth;
+        height = rootEl.clientHeight;
     } else {
-        return el.scrollLeft >= el.scrollWidth - el.clientWidth - offset;
+        width = scrollEl.clientWidth;
+        height = scrollEl.clientHeight;
+    }
+
+    const {scrollTop, scrollLeft, scrollHeight, scrollWidth} = scrollEl;
+
+    if (direct === "vertical") {
+        return scrollTop >= scrollHeight - height - offset;
+    } else {
+        return scrollLeft >= scrollWidth - width - offset;
     }
 }
 
