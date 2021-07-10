359dece2fc3f92045368f8759fd2a98198578772
dyh <xinzon32177@163.com>
2021-07-10 15:39:19 +0800

    refactor(common.ts): parseCmdParams

--- src/common.ts
+++ src/common.ts
@@ -500,55 +500,54 @@ export function likeKeys(target: object | Map<string, any>, key: string | RegExp
 
 /**
  * 命令行的参数转为Map
+ * @notice 部分命令行工具中"--"是全写，"-"是缩写 这里未分
  * @param arr 命令行参数数组
  * @param prefix 前缀 --d --f 前缀是"--"
  * @param defaultKey 如果前面没有变量名那么使用默认
  */
 export function parseCmdParams(arr: string[], prefix = "-", defaultKey = "default"): Map<string, string[] | string | boolean> {
-    const list = arr.slice();
-    let currentKey = defaultKey;
-    const isKeyReg = new RegExp(`^${prefix}`);
     const eqReg = /([^=]+)=([\s\S]+)?/;
+    const isKeyReg = new RegExp(`^${prefix}`);
+    const list = arr.slice();
     const map: ReturnType<typeof parseCmdParams> = new Map();
+    let currentKey = defaultKey;
+    let item: string;
 
-    function parseKey(key: string): string {
+    function setKey(): void {
+        let key = item.replace(isKeyReg, "");
         if (eqReg.test(key)) {
             key = RegExp.$1;
             const value = RegExp.$2;
             value && list.unshift(value);
         }
-        return key;
-    }
-
-    function setValue(currentValue?: string[] | string | boolean) {
-        switch (typeOf(currentValue)) {
-            case "undefined":
-            case "boolean":
-                map.set(currentKey, it);
-                break;
-            case "array":
-                (currentValue as Array<string>).push(it);
-                break;
-            default:
-                map.set(currentKey, [currentValue as string, it]);
-        }
-    }
-
-    let it;
-
-    function setKey() {
-        currentKey = parseKey(it.replace(isKeyReg, ""));
+        currentKey = key;
         if (!map.has(currentKey)) {
             map.set(currentKey, true);
         }
     }
 
-    while (it = list.shift()) {
-        if (isKeyReg.test(it)) {
+    // fullFight
+    function setValue() {
+        const existValue = map.get(currentKey);
+        switch (typeOf(existValue)) {
+            case "undefined":
+            case "boolean":
+                map.set(currentKey, item);
+                break;
+            case "array":
+                (existValue as Array<string>).push(item);
+                break;
+            default:
+                map.set(currentKey, [existValue as string, item]);
+        }
+    }
+
+    while (item = list.shift()!) {
+        if (isKeyReg.test(item)) {
             setKey();
             continue;
         }
-        setValue(map.get(currentKey));
+        setValue();
     }
     return map;
 }
