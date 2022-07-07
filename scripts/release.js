dd5ae3a734e2d89b1f84645c385f44d4b8e73f84
dyh_a <xinzon32177@163.com>
2022-07-07 23:55:25 +0800

    refactor: release 收拢各种变量

--- scripts/release.js
+++ scripts/release.js
@@ -5,25 +5,30 @@ const { prompt } = require('enquirer');
 
 const args = require('minimist')(process.argv.slice(2));
 
-const currentVersion = pkg.version;
-
 const pkgs = fs.readdirSync(path.resolve(__dirname, '../packages'));
 
-let targetVersion = args._[0];
+const targetVersion = args._[0];
 
-console.log(targetVersion);
-
-async function main() {
+async function getConfig() {
+  const config = {
+    pkgs,
+    targetVersion,
+    currentVersion: pkg.version,
+  };
   if (!targetVersion) {
-    const { release } = await prompt({
+    ({ release: config.targetVersion } = await prompt({
       type: 'select',
       name: 'release',
       choices: ['patch', 'minor', 'major']
-        .map((i) => `${i} (${currentVersion})`)
+        .map((i) => `${i} (${config.currentVersion})`)
         .concat(['custom']),
-    });
-    console.log(release);
+    }));
   }
 }
 
-main();
+async function setup() {
+  const config = await getConfig();
+  console.log(config);
+}
+
+setup();
