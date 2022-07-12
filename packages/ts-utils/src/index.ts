1f60f5fd70165034ec181bd860b434869ba29ab8
DYH <xinzon32177@163.com>
2022-07-12 18:13:06 +0800

    feat(ts-utils): global global

--- packages/ts-utils/src/index.ts
+++ packages/ts-utils/src/index.ts
@@ -2,3 +2,11 @@ export * from '@mxssfd/core';
 export * from '@mxssfd/types';
 export * from '@mxssfd/bom';
 export * from '@mxssfd/dom';
+
+import * as all from './index';
+
+declare global {
+  interface Window {
+    tsUtils: typeof all;
+  }
+}
