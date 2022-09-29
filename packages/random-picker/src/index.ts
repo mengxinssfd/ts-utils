9217e68a360b42c7a3ba5254383ff9fc0f67f069
dyh_a <xinzon32177@163.com>
2022-09-29 17:57:15 +0800

    chore: package random-picker 优化 分离types

--- packages/random-picker/src/index.ts
+++ packages/random-picker/src/index.ts
@@ -1,18 +1,5 @@
 import type { Tuple } from '@mxssfd/types';
-
-type WeightFn = (weightTotal: number) => number;
-type RangeItem<T> = {
-  range: number;
-} & (WeightFnItem<T> | WeightItem<T>);
-
-interface WeightItem<T> {
-  weights: number;
-  item: T;
-}
-interface WeightFnItem<T> {
-  weights: WeightFn;
-  item: T;
-}
+import type { PoolItem, WeightFn, WeightsFnItem, WeightsItem, OptionList } from './types';
 
 /**
  * 工具类库：从选项池中随机挑选n个选项
@@ -33,7 +20,7 @@ interface WeightFnItem<T> {
  * picker.reset();
  *
  * // 添加选项
- * picker.option(6);
+ * picker.option(6); lottery-pool
  *
  * // 恢复初始选项，去除picker.option方法添加的选项
  * picker.resetBySeed();
@@ -43,17 +30,17 @@ export class RandomPicker<T> {
   /**
    * 选项池
    */
-  protected optionsPool: RangeItem<T>[] = [];
+  protected optionsPool: PoolItem<T>[] = [];
   /**
    * 全部选项，包含后面option添加的
    */
-  protected options: Array<WeightFnItem<T> | WeightItem<T>> = [];
+  protected optionList: OptionList<T> = [];
 
   constructor(
     /**
      * 初始选项，不包含后面option添加的
      */
-    protected readonly seed: [item: T, weights: number | WeightFn][] | T[] = [],
+    protected readonly seed: [option: T, weights: number | WeightFn][] | T[] = [],
   ) {
     // 禁止在new RandomPicker([1,2,3])后继续对seed数组增减，
     // 如果需要增减那么直接再new一个实例就好
@@ -68,26 +55,28 @@ export class RandomPicker<T> {
    * @protected
    */
   protected handleSeed(): void {
-    this.options = this.seed.map((item) => {
-      return Array.isArray(item) ? { item: item[0], weights: item[1] ?? 1 } : { item, weights: 1 };
-    }) as Array<WeightFnItem<T> | WeightItem<T>>;
+    this.optionList = this.seed.map((item) => {
+      return Array.isArray(item)
+        ? { option: item[0], weights: item[1] ?? 1 }
+        : { item, weights: 1 };
+    }) as Array<WeightsFnItem<T> | WeightsItem<T>>;
   }
 
   /**
    * 计算各个选项的所占的百分比及所处位置，并生成选项池
    * @protected
    */
-  protected generateOptionsPool(list = this.options): void {
+  protected generateOptionsPool(list = this.optionList): void {
     // 计算百分比
     const [weights, fnList] = list.reduce(
       ([weightTotal, fnList], cur) => {
         if (typeof cur.weights === 'number') {
           return [weightTotal + cur.weights, fnList];
         }
-        fnList.push(cur as WeightFnItem<T>);
+        fnList.push(cur as WeightsFnItem<T>);
         return [weightTotal, fnList];
       },
-      [0, [] as WeightFnItem<T>[]],
+      [0, [] as WeightsFnItem<T>[]],
     );
     // WeightFn如果返回的是百分比，那么这个百分比是相对于固定weight的累积百分比，
     // 也就是说动态百分比是相对于静态百分比的
@@ -105,20 +94,36 @@ export class RandomPicker<T> {
   }
 
   /**
-   * 添加选项
-   * @param item 选项
+   * 添加单个选项，如果需要添加多个则推荐使用options
+   * @param option 选项
    * @param weights 权重
    * @return {this}
    */
-  option(item: T, weights: number | WeightFn): this {
-    this.options.push({ weights, item } as WeightFnItem<T> | WeightItem<T>);
+  option(option: T, weights: number | WeightFn): this {
+    this.optionList.push({ weights, option } as WeightsFnItem<T> | WeightsItem<T>);
+    this.generateOptionsPool();
+    return this;
+  }
+  /**
+   * 添加多个选项
+   * @param options [[选项, 权重], [选项, 权重], ...]
+   * @return {this}
+   */
+  options(options: Array<[option: T, weights: number | WeightFn]>): this {
+    options.forEach(([option, weights]) =>
+      this.optionList.push({ weights, option } as WeightsFnItem<T> | WeightsItem<T>),
+    );
     this.generateOptionsPool();
     return this;
   }
 
-  protected getRandomItem(list = this.optionsPool): T {
+  /**
+   * 从选项池中获取随机选项
+   * @protected
+   */
+  protected getRandomOption(): T {
     const range = ~~(Math.random() * 1000_000) / 10_000;
-    return (list.find((item) => item.range > range) as RangeItem<T>).item;
+    return (this.optionsPool.find((item) => item.range > range) as PoolItem<T>).option;
   }
 
   /**
@@ -139,12 +144,12 @@ export class RandomPicker<T> {
   pick<N extends number>(count: N): Tuple<T, N>;
   pick(count = 1): T[] | T {
     if (count === 1) {
-      return this.getRandomItem();
+      return this.getRandomOption();
     }
 
     return Array(count)
       .fill(void 0)
-      .map(() => this.getRandomItem());
+      .map(() => this.getRandomOption());
   }
 
   /**
@@ -166,34 +171,34 @@ export class RandomPicker<T> {
   take<N extends number>(count: N): Tuple<T | void, N>;
   take(count = 1): void | T | T[] {
     if (!this.poolLen) return;
-    const removeItem = (item: T, list = this.optionsPool) => {
-      list = list.filter((it) => it.item !== item);
+    const removeOption = (option: T, list = this.optionsPool) => {
+      list = list.filter((it) => it.option !== option);
       this.generateOptionsPool(list);
     };
     if (count === 1) {
-      const item = this.getRandomItem();
-      removeItem(item);
-      return item;
+      const option = this.getRandomOption();
+      removeOption(option);
+      return option;
     }
 
     return Array(Math.min(count, this.poolLen))
       .fill(void 0)
       .map(() => {
-        const item = this.getRandomItem();
-        removeItem(item);
-        return item;
+        const option = this.getRandomOption();
+        removeOption(option);
+        return option;
       });
   }
 
   /**
    * 移除一个选项
-   * @param item
+   * @param option 选项
    * @return 操作成功或失败
    */
-  remove(item: T): boolean {
-    const index = this.options.findIndex((it) => it.item === item);
+  remove(option: T): boolean {
+    const index = this.optionList.findIndex((it) => it.option === option);
     if (index === -1) return false;
-    this.options.splice(index, 1);
+    this.optionList.splice(index, 1);
     this.generateOptionsPool();
     return true;
   }
@@ -221,7 +226,7 @@ export class RandomPicker<T> {
    * @return 选项数组长度
    */
   get len(): number {
-    return this.options.length;
+    return this.optionList.length;
   }
 
   /**
@@ -232,9 +237,37 @@ export class RandomPicker<T> {
   }
 
   /**
-   * @returns 由item、weights组成的元组所组成的选项数组，可以用于new RandomPicker(picker.export())
+   * 获取选项池剩余的选项
    */
-  export(): [item: T, weights: number | WeightFn][] {
-    return this.options.map((item) => [item.item, item.weights]);
+  getPoolOptions(): T[] {
+    return this.optionsPool.map((item) => item.option);
+  }
+
+  /**
+   * 获取选项选中的几率
+   * @param  option 选项
+   * @return {number} 几率
+   */
+  getOptionRate(option: T): number {
+    const pool = this.optionsPool;
+    const index = pool.findIndex((it) => it.option === option);
+    if (index === -1) return 0;
+    return pool[index].range - (pool[index - 1]?.range ?? 0);
+  }
+
+  /**
+   * 导出所有选项
+   * @returns 由option、weights组成的元组所组成的选项数组，可以用于new RandomPicker(picker.export())
+   */
+  export(): [option: T, weights: number | WeightFn][] {
+    return this.optionList.map((item) => [item.option, item.weights]);
+  }
+
+  /**
+   * 导出选项池的所有选项
+   * @returns 由option、weights组成的元组所组成的选项数组，可以用于new RandomPicker(picker.export())
+   */
+  exportPool(): [option: T, weights: number | WeightFn][] {
+    return this.optionsPool.map((item) => [item.option, item.weights]);
   }
 }
---
+++ packages/random-picker/src/types.ts
@@ -0,0 +1,17 @@
+export type WeightFn = (weightTotal: number) => number;
+
+export interface WeightsItem<T> {
+  weights: number;
+  option: T;
+}
+
+export interface WeightsFnItem<T> {
+  weights: WeightFn;
+  option: T;
+}
+
+export type PoolItem<T> = {
+  range: number;
+} & (WeightsFnItem<T> | WeightsItem<T>);
+
+export type OptionList<T> = Array<WeightsFnItem<T> | WeightsItem<T>>;
