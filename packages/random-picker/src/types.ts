0a4e45510f4dfd42e74e36ac19828ae30d470437
dyh_a <xinzon32177@163.com>
2022-10-01 15:08:37 +0800

    docs(random-picker): types

--- packages/random-picker/src/types.ts
+++ packages/random-picker/src/types.ts
@@ -1,20 +1,35 @@
 export type WeightFn = (weightTotal: number) => number;
 
+/**
+ * 静态权重
+ */
 export interface WeightsItem<T> {
   weights: number;
   option: T;
 }
 
+/**
+ * 动态权重
+ */
 export interface WeightsFnItem<T> {
   weights: WeightFn;
   option: T;
 }
 
+/**
+ * 选项池数组item
+ */
 export type PoolItem<T> = {
   range: number;
 } & (WeightsFnItem<T> | WeightsItem<T>);
 
+/**
+ * 选项列表item
+ */
 export type OptionListItem<T> = WeightsFnItem<T> | WeightsItem<T>;
+/**
+ * 选项列表
+ */
 export type OptionList<T> = Array<OptionListItem<T>>;
 
 /**
@@ -22,4 +37,7 @@ export type OptionList<T> = Array<OptionListItem<T>>;
  */
 export type OptionWeightsTuple<T> = [option: T, weights: number | WeightFn];
 
+/**
+ * Seed 数组
+ */
 export type Seed<T> = Array<OptionWeightsTuple<T> | T>;
