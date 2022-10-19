cbcca8b5dcc57cc066bdd3a254f0673b587c0ad6
dyh_a <xinzon32177@163.com>
2022-10-20 02:04:09 +0800

    refactor(ts-utils): 重构RandomPicker导出,export * from '@mxssfd/random-picker'; 改为import * as RandomPicker from '@mxssfd/random-picker';export { RandomPicker };
    BREAKING CHANGE: 外部引用需要import {RandomPicker} from 'ts-utils';
    RandomPicker.RandomPicker

--- packages/ts-utils/src/index.ts
+++ packages/ts-utils/src/index.ts
@@ -2,4 +2,5 @@ export * from '@mxssfd/core';
 export * from '@mxssfd/types';
 export * from '@mxssfd/bom';
 export * from '@mxssfd/dom';
-export * from '@mxssfd/random-picker';
+import * as RandomPicker from '@mxssfd/random-picker';
+export { RandomPicker };
