763de978008418c87dcc5766be9fe03b9bcc0a68
dyh_a <xinzon32177@163.com>
2022-11-23 16:06:16 +0800

    docs: removeStrByNum

--- packages/core/src/string.ts
+++ packages/core/src/string.ts
@@ -193,10 +193,10 @@ chinese2Number.numbers = [...numberArr];
 /**
  * 从字符串中删除指定字符串(from)中重复的第n(num)个字符串(str)
  * @example
- * removeSlashByNum("123/456/78", 2, "\/"); // "123/45678"
- * @param from
- * @param num
- * @param removeStr
+ * removeStrByNum("123/456/78", 2, "\/"); // "123/45678"
+ * @param from 原字符串
+ * @param num 要移除的字符串出现的第几次
+ * @param removeStr 要查找并移除的字符串
  */
 export function removeStrByNum(from: string, num: number, removeStr: string): string {
   let times = 1;
